46193fa4f0740d81d132e4ddce122fadb484ccbf
Jaime <lcalmsky@gmail.com>
2021-09-04 03:22:24 +0900

    Initial commit

---
+++ src/main/java/io/lcalmsky/simplegrpcserver/SimpleGrpcServerApplication.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.simplegrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGrpcServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/lcalmsky/simplegrpcserver/SimpleGrpcServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lcalmsky.simplegrpcserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGrpcServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
